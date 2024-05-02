5142c4b51e33aec0a56bf43956d784bb67c80e65
Carlos <gomec1@bfh.ch>
2024-05-02 16:08:20 +0200

    Hochladen Deliverable 3 PP und aufräumen Ordner

---
+++ Angular /two4four/src/app/landingpage/landingpage.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-landingpage',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <section class="hintergrund">
+      <div class="suchbox1"></div>
+
+      <div class="kreismitte"></div>
+
+      <div class="suchbox2"></div>
+      <!--
+        <img
+          class="tiersittersuchebox"
+          src="/assets/tier-sitter-suche.png"
+          alt="Box Tier Sitter Suche"
+        />
+      </div>
+--></section>
+  `,
+  styleUrl: './landingpage.component.css',
+})
+export class LandingpageComponent {}
