5287600c8203f8f8162dd082b9f43e820e7fb906
Carlos <gomec1@bfh.ch>
2024-05-10 11:57:45 +0200

    Verschiebung und Löschung Dateien

--- Angular /two4four/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
-import routeConfig from './app/app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-
-bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routeConfig), provideAnimationsAsync()],
-}).catch((err) => console.error(err));
--- Angular Tutorial/homes-app/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { bootstrapApplication } from "@angular/platform-browser";
-import { AppComponent } from "./app/app.component";
-import { provideRouter } from "@angular/router";
-import routeconfig from "./app/routes";
-
-bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routeconfig)],
-}).catch((err) => console.error(err));
