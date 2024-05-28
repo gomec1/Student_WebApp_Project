176084a248e4c10bc1d27906fc7710f76f1bb316
Carlos <gomec1@bfh.ch>
2024-05-28 16:01:32 +0200

    versuch Bilder ans Backend zu senden, ohne Erfolg

--- frontend/src/app/registrieren/registrieren.component.ts
+++ frontend/src/app/registrieren/registrieren.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { RouterModule, Router } from "@angular/router";
 import { FormsModule } from "@angular/forms";
 import { NgIf } from "@angular/common";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { HttpClientModule } from "@angular/common/http";
 import { MatSnackBar } from "@angular/material/snack-bar";
 
@@ -10,7 +10,155 @@ import { MatSnackBar } from "@angular/material/snack-bar";
   selector: "app-registrieren",
   standalone: true,
   imports: [RouterModule, NgIf, FormsModule, HttpClientModule],
-  templateUrl: "./registrieren.component.html",
+  template: `
+    <section class="hintergrundunten">
+      <form class="form-container" (ngSubmit)="onSubmit()">
+        <h1 class="überschrift">Registrieren</h1>
+        <div class="fotohochladenkreis">
+          <img
+            class="profilzeichen"
+            src="/assets/profil-zeichen.png"
+            alt="Profil Zeichen"
+          />
+          <h1 class="schriftfotohochladen">Profilfoto hinzufügen</h1>
+        </div>
+        <input
+          class="profilfoto"
+          id="profilfoto"
+          type="file"
+          (change)="onFileSelected($event, 'profilfoto')"
+        />
+
+        <!-- Add ngSubmit event -->
+        <div class="eingabefelder-container">
+          <label for="name" class="schrift">Name</label>
+          <input
+            class="eingabefelder"
+            name="name"
+            type="text"
+            [(ngModel)]="name"
+          />
+        </div>
+
+        <div class="eingabefelder-container">
+          <label for="username" class="schrift">Username</label>
+          <input
+            class="eingabefelder"
+            id="username"
+            name="username"
+            type="text"
+            [(ngModel)]="username"
+          />
+        </div>
+
+        <div class="eingabefelder-container">
+          <label for="ausweis" class="schrift">Ausweis</label>
+          <input
+            class="eingabefeldausweis"
+            name="ausweis"
+            type="file"
+            (change)="onFileSelected($event, 'ausweis')"
+          />
+        </div>
+
+        <div class="eingabefelder-container">
+          <label for="vorname" class="schrift">Vorname</label>
+          <input
+            class="eingabefelder"
+            name="vorname"
+            type="text"
+            [(ngModel)]="vorname"
+          />
+        </div>
+
+        <div class="eingabefelder-container">
+          <label for="phone" class="schrift">Telefonnummer</label>
+          <input
+            class="eingabefelder"
+            type="text"
+            id="phone"
+            name="phone"
+            placeholder="077 123 45 67"
+            [(ngModel)]="telefonnummer"
+          />
+        </div>
+
+        <div class="eingabefelder-container">
+          <label for="geburtsdatum" class="schrift">Geburtstdatum</label>
+          <input
+            class="eingabefelder"
+            name="geburtsdatum"
+            type="text"
+            [(ngModel)]="geburtsdatum"
+          />
+        </div>
+
+        <div class="eingabefelder-container">
+          <label for="strasse+nr" class="schrift">Strasse + Nr</label>
+          <input
+            class="eingabefelder"
+            name="strasse+nr"
+            type="text"
+            [(ngModel)]="strasseNr"
+          />
+        </div>
+
+        <div class="eingabefelder-container">
+          <label for="email" class="schrift">Email</label>
+          <input
+            class="eingabefelder"
+            id="email"
+            name="email"
+            type="email"
+            [(ngModel)]="email"
+          />
+        </div>
+
+        <div class="eingabefelder-container">
+          <label for="password" class="schrift">Passwort</label>
+          <input
+            class="eingabefelder"
+            id="password"
+            name="password"
+            type="password"
+            [(ngModel)]="password"
+          />
+        </div>
+
+        <div class="eingabefelder-container">
+          <label for="postleitzahl" class="schrift">Postleitzahl</label>
+          <input
+            class="eingabefelder"
+            name="postleitzahl"
+            type="text"
+            [(ngModel)]="postleitzahl"
+          />
+        </div>
+
+        <div class="eingabefelder-container">
+          <label for="ort" class="schrift">Ort</label>
+          <input
+            class="eingabefelder"
+            name="ort"
+            type="text"
+            [(ngModel)]="ort"
+          />
+        </div>
+
+        <div class="eingabefelder-container">
+          <label for="rolle" class="schrift">Rolle</label>
+          <input list="rolle" name="rolle" [(ngModel)]="rolle" />
+          <datalist id="rolle">
+            <option value="Ich bin Tiersitter"></option>
+            <option value="Ich bin Tierbesitzer"></option>
+            <option value="beides"></option>
+          </datalist>
+        </div>
+
+        <input class="submit" type="submit" value="Submit" />
+      </form>
+    </section>
+  `,
   styleUrl: "./registrieren.component.css",
 })
 export class RegistrierenComponent implements OnInit {
@@ -28,7 +176,6 @@ export class RegistrierenComponent implements OnInit {
   geburtsdatum: string = "";
   profilfoto: File | null = null;
   ausweis: File | null = null;
-  backgroundImage: string = "";
 
   constructor(
     private http: HttpClient,
@@ -40,10 +187,16 @@ export class RegistrierenComponent implements OnInit {
 
   // Diese Methode wird aufgerufen, wenn der Benutzer ein Profilbild oder ausweis hochlädt
   onFileSelected(event: Event, type: string) {
+    console.log("onFileSelected wurde aufgerufen");
+
     const target = event.target as HTMLInputElement;
     if (target.files && target.files.length > 0) {
       const file = target.files[0];
       const fileType = file.type;
+      this.profilfoto = file;
+      console.log("Ausgewählte Datei:", file);
+      console.log("Dateityp:", fileType);
+
       const validImageTypes = [
         "image/jpeg",
         "image/png",
@@ -60,26 +213,22 @@ export class RegistrierenComponent implements OnInit {
       if (validImageTypes.includes(fileType)) {
         if (type === "profilfoto") {
           this.profilfoto = file;
+          console.log("Profilfoto gesetzt:", this.profilfoto);
         } else if (type === "ausweis") {
           this.ausweis = file;
+          console.log("Ausweis gesetzt:", this.ausweis);
         }
       } else {
+        console.log("Ungültiger Dateityp");
         alert("Bitte laden Sie eine Datei im unterstützten Format hoch.");
       }
+    } else {
+      console.log("Keine Datei ausgewählt");
     }
   }
 
-  // onFileChange(event: Event) {
-  //   const target = event.target as HTMLInputElement;
-  //   if (target.files && target.files.length > 0) {
-  //     const file = target.files[0];
-  //     this.profilfoto = file;
-  //   }
-  // }
-
   onSubmit() {
     debugger;
-
     const formData = new FormData();
     formData.append("username", this.username);
     formData.append("vorname", this.vorname);
@@ -92,24 +241,42 @@ export class RegistrierenComponent implements OnInit {
     formData.append("strasseNr", this.strasseNr);
     formData.append("postleitzahl", this.postleitzahl);
     formData.append("geburtsdatum", this.geburtsdatum);
-    if (this.profilfoto) {
-      formData.append("file", this.profilfoto, this.profilfoto.name);
-    }
-
-    // if (this.ausweis) {
-    //   formData.append("files.ausweis", this.ausweis);
-    // }
 
     this.http
       .post("http://localhost:1337/api/auth/local/register", formData)
       .subscribe(
-        (response) => {
+        (response: any) => {
           console.log(response);
           this.snackBar.open("Registrierung erfolgreich", "OK", {
             duration: 3000,
             verticalPosition: "top",
             horizontalPosition: "center",
           });
+          // Save JWT token and user ID
+          const jwtToken = response.jwt;
+          const userId = response.user.id;
+          console.log("JWT token:", jwtToken);
+          console.log("User ID:", userId);
+          // Use the token and user ID as needed
+          // ...
+
+          const profilfotoFormData = new FormData();
+          if (this.profilfoto) {
+            profilfotoFormData.append("files", this.profilfoto);
+            profilfotoFormData.append("ref", "User"); // the name of your content type
+            profilfotoFormData.append("refId", userId); // the id of the entry
+            profilfotoFormData.append("field", "profilfoto"); // the name of your field
+          }
+          this.http
+            .post("http://localhost:1337/api/upload", profilfotoFormData, {
+              headers: new HttpHeaders({
+                Authorization: `Bearer ${jwtToken}`,
+              }),
+            })
+            .subscribe(
+              (response) => console.log(response),
+              (error) => console.log(error)
+            );
           this.router.navigate(["/login"]);
         },
         (error: any) => {
@@ -117,18 +284,4 @@ export class RegistrierenComponent implements OnInit {
         }
       );
   }
-
-  /* Foto hochladen und wird ersetzt Funktion */
-  //   backgroundImage: string = ""; // Declare the 'backgroundImage' property
-  //   handleFileInput(event: Event) {
-  //     const fileInput = event.target as HTMLInputElement;
-  //     if (fileInput.files && fileInput.files.length > 0) {
-  //       const file = fileInput.files[0];
-  //       const reader = new FileReader();
-  //       reader.onloadend = () => {
-  //         this.backgroundImage = reader.result as string;
-  //       };
-  //       reader.readAsDataURL(file);
-  //     }
-  //   }
 }
